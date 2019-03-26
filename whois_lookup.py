7e7a1ad7c6c15a37374d7389555a4cf910a128c5
prashasy <prashasyashok@gmail.com>
2019-03-27 00:04:13 +0530

    created function for whois lookup

--- whois_lookup.py
+++ whois_lookup.py
@@ -1,7 +1,11 @@
 import whois
 
+def whois(url):
+	w=whois.whois(url)
+	print(w['expiration_date'])
+	print(w['domain_name'])
+
+
 print("Enter website name")
 url=input()
-w=whois.whois(url)
-print(w['expiration_date'])
-print(w['domain_name'])
+whois(url)
