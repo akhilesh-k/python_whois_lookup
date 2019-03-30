a0da551d67b9aa6d793e3b461558a87c380aeec7
prashasy <prashasyashok@gmail.com>
2019-03-30 23:13:12 +0530

    minor changes

--- whois_lookup.py
+++ whois_lookup.py
@@ -4,7 +4,7 @@ import socket
 def whois_lookup(url):
 	w=whois.whois(url)
 	print("\nDomain Expiry Date: {}".format(w['expiration_date']))
-	print("Domain Nmae: {}\n".format(w['domain_name']))
+	print("Domain Name: {}\n".format(w['domain_name']))
 	print("IP Address: {}\n".format(socket.gethostbyname(url)))
 
 print("Enter website name")
