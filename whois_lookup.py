8fc9aa95e379d6cde1296769896c979247728d72
prashasy <prashasyashok@gmail.com>
2019-03-30 15:02:41 +0530

    major update: added functionality to lookup ip address

--- whois_lookup.py
+++ whois_lookup.py
@@ -1,10 +1,11 @@
 import whois
+import socket
 
 def whois_lookup(url):
 	w=whois.whois(url)
 	print("\nDomain Expiry Date: {}".format(w['expiration_date']))
 	print("Domain Nmae: {}\n".format(w['domain_name']))
-
+	print(socket.gethostbyname(url))
 
 print("Enter website name")
 url=input()
