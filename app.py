b9ec24101a08ebc3fbe473a1d09d6242fa8137fd
akhilesh-k <akhilesh_k@outlook.com>
2019-04-07 16:19:56 +0530

    updated by arnav

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
-#import whois_lookup
+import whois_lookup
  
 app = Flask(__name__)
  
@@ -14,10 +14,11 @@ def home():
 @app.route("/", methods=['POST'])
 def search():
     url=request.form['url']
+    a = whois_lookup.whois_lookup(url)
     #a=whois_lookup.whois_lookup(url)
-    a={'2019-06-23 07:31:46','CYBERVIE.COM','13.126.15.3'}
+    #a={'2019-06-23 07:31:46','CYBERVIE.COM','13.126.15.3'}
     return render_template("index.html",details=a)
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4000)
+    app.run(debug=True,host='0.0.0.0', port=5000)
--- whois_lookup.py
+++ whois_lookup.py
@@ -3,14 +3,14 @@ import socket
 
 def whois_lookup(url):
 	w=whois.whois(url)
-	# print("\nDomain Expiry Date: {}".format(w['expiration_date']))
-	# print("Domain Name: {}\n".format(w['domain_name']))
-	# print("IP Address: {}\n".format(socket.gethostbyname(url)))
+	print("\nDomain Expiry Date: {}".format(w['expiration_date']))
+	print("Domain Name: {}\n".format(w['domain_name']))
+	print("IP Address: {}\n".format(socket.gethostbyname(url)))
 	e=w['expiration_date']
 	d=w['domain_name']
 	ip=socket.gethostbyname(url)
 	a=[e,d,ip]
-	print("Hello1")
+	#print("Hello1")
 	return a
 
-a=whois_lookup("www.cybervie.com")
+#a=whois_lookup("www.cybervie.com")
